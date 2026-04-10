6f3340574f969ec60442fe8dad01192dde6585f8
Markus Kammin <markus.kammin@haiilo.com>
2026-04-10 17:41:21 +0200

    initial setup of component structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@styles": path.resolve(__dirname, "./src/styles"),
+        "@layouts": path.resolve(__dirname, "./src/layouts"),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          loadPaths: [path.resolve(__dirname, "./src")],
+        },
+      },
+    },
+  },
+});
