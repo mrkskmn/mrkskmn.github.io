d60bd04792f1cb876108750d92c9973ae3563586
Markus Kammin <markus.kammin@haiilo.com>
2026-04-28 12:03:28 +0200

    small tweaks

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 export const home = "Home"
 export const notes = "Selected work"
 export const cv = "CV"
+export const cvURL = "https://drive.google.com/file/d/1kzRzabQmUiNvbPihUjbR9mPaBstz23NC/view?usp=sharing"
 export const contact = "Get in touch"
 export const linkedin = "LinkedIn"
 export const github = "GitHub"
@@ -10,3 +11,9 @@ export const imprint = "Imprint"
 // Meta tags
 export const description = "Markus Kammin, design leader helping companies ship better products and establish design systems. Head of UX at Haiilo, based in Hamburg, Germany.";
 export const ogImage = "/og-image.jpg";
+
+// ENVs
+export const activateCases = false;
+export const activateCV = true;
+export const cursorVisibleByDefault = false;
+export const activateBrands = false;
